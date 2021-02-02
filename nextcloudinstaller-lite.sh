82b61f56282cfc26dc3f559ed346ed156188a513
Milanotje <69339647+Milanotje@users.noreply.github.com>
2021-02-02 16:22:00 +0100

    Add files via upload

---
+++ nextcloudinstaller-lite.sh
@@ -0,0 +1,60 @@
+echo "* Checking for updates.."
+sudo apt update -y
+
+
+echo "* Installing net-tools.."
+sudo apt install net-tools -y
+echo "* net-tools installed!"
+
+
+
+
+echo "* Installing Apache web server.."
+sudo apt install -y apache2 libapache2-mod-php bzip2
+echo "* Apache Web server installed!"
+
+
+
+echo "* Installing PHP Modules for nextcloud.."
+sudo apt install -y php-gd php-json php-mysql php-curl php-mbstring php-intl php-imagick php-xml php-zip php-sqlite3
+echo "* PHP modules succesfully installed!"
+
+
+echo "* Enabling mod_rewrite for nextcloud to function properly.."
+sudo a2enmod rewrite
+echo "* mod_rewrite succesfully enabled!"
+
+echo "* Enabling additional Apache modules.."
+sudo a2enmod headers
+sudo a2enmod dir
+sudo a2enmod env
+sudo a2enmod mime
+echo "* Apache Modules succesfully enabled!"
+
+
+echo "* Restarting apache.."
+sudo systemctl restart apache2
+echo "* apache succesfully restarted!"
+
+
+
+
+echo "* Downloading and extracting nextcloud files .. "
+ 
+sudo apt install wget -y
+
+cd /var/www/html
+
+wget https://download.nextcloud.com/server/installer/setup-nextcloud.php
+
+sudo chown www-data:www-data /var/www/html/ -R
+
+sudo chmod 775 -R /var/www/html/
+sudo systemctl restart apache2
+
+echo "* Done installing nextcloud"
+
+echo "* nextcloud lite installation completed"
+echo "* Acces the server by typing <ip>/setup-nextcloud.php in your browser"
+echo "* Because the scipt is in beta there might've been a few flaws"
+echo "* For now it only uses SQLite, MariaDB Isn't supported yet" 
